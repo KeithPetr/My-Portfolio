ea5b4da8214ce73a19e8b8ccf6c4cefa8ac4aeb5
KeithPetr <keith.petryshyn@gmail.com>
2023-04-17 12:39:19 -0300

    Can navigate back to index page through mobile nav bar

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const hamburgerIcon = document.getElementById('hamburger-icon');
 const navBarTwo = document.querySelector('.nav-bar-two');
 const body = document.querySelector('body')
 const navLinks = document.querySelectorAll('.nav-item');
+const navLinksTwo = document.querySelectorAll('.nav-item-two')
 
 hamburgerIcon.addEventListener('click', () => {
   navBarTwo.classList.toggle('open');
@@ -17,6 +18,21 @@ body.addEventListener('click', function(event) {
 
 if (location.pathname === '/' || location.pathname === "/index.html") {
   navLinks.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault()
+      const href = link.getAttribute('href');
+      const target = document.querySelector(href);
+      const offsetTop = target.offsetTop;
+      window.scrollTo({
+        top: offsetTop,
+        behavior: 'smooth'
+      });
+    });
+  });
+}
+
+if (location.pathname === '/' || location.pathname === "/index.html") {
+  navLinksTwo.forEach(link => {
     link.addEventListener('click', e => {
       e.preventDefault()
       const href = link.getAttribute('href');
